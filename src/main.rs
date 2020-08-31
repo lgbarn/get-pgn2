d3cbab73793b9aa32dc9193da7b5fed8395195e3
Luther Barnum <luther.barnum@gmail.com>
2020-08-31 12:33:16 -0400

    Deserialized JSON to Struct

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
-use std::io::{stdout, Write};
+use error_chain::error_chain;
+use std::io::Read;
+use serde_json;
+use serde::{Deserialize};
 
-use curl::easy::Easy;
-
-
-fn main() {
-    let mut easy = Easy::new();
-easy.url("https://api.chess.com/pub/player/lgbarn/games/archives").unwrap();
-easy.write_function(|data| {
-    stdout().write_all(data).unwrap();
-    Ok(data.len())
-}).unwrap();
-easy.perform().unwrap();
-
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
 }
+
+#[derive(Deserialize, Debug)]
+struct Archive {
+    archives: Vec<String>,
+}
+
+fn main() -> Result<()> {
+    let mut res = reqwest::blocking::get("https://api.chess.com/pub/player/lgbarn/games/archives")?;
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
+
+    let deserialized: Archive = serde_json::from_str(&body).unwrap();
+
+    println!("deserialized = {:?}", deserialized);
+
+    Ok(())
+}
+
+
