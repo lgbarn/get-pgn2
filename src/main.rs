4826604a4420216be99197772114d18dcda25beb
Luther Barnum <luther.barnum@gmail.com>
2020-08-31 20:36:14 -0400

    Added get games routine to main

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use error_chain::error_chain;
 use std::io::Read;
 use serde_json;
 use serde::{Deserialize};
+//use curl::easy::Easy;
+//use curl::easy;
+//use restson::RestClient;
+
 
 error_chain! {
     foreign_links {
@@ -15,6 +19,12 @@ struct Archive {
     archives: Vec<String>,
 }
 
+impl Archive {
+    pub fn get_months(self)  -> Vec<String> {
+        self.archives
+    }
+}
+
 fn main() -> Result<()> {
     let mut res = reqwest::blocking::get("https://api.chess.com/pub/player/lgbarn/games/archives")?;
     let mut body = String::new();
@@ -22,7 +32,16 @@ fn main() -> Result<()> {
 
     let deserialized: Archive = serde_json::from_str(&body).unwrap();
 
-    println!("deserialized = {:?}", deserialized);
+    println!("{:?}", deserialized.get_months());
+
+   
+    let mut res = reqwest::blocking::get("https://api.chess.com/pub/player/lgbarn/games/2010/05/pgn")?;
+    let mut data = String::new();
+    res.read_to_string(&mut data)?;
+
+    
+    println!("{}", data);
+    
 
     Ok(())
 }
