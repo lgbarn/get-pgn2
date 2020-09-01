d850210c01dff20945c92d06bded0a3a1faf4572
Luther Barnum <luther.barnum@gmail.com>
2020-09-01 10:30:04 -0400

    Added ability to specify filename

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use clap::{App, Arg};
 use error_chain::error_chain;
 use serde::Deserialize;
 use serde_json;
-use std::io::Read;
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{Read, Write};
+//use std::io::prelude::*;
 
 error_chain! {
     foreign_links {
@@ -11,7 +12,6 @@ error_chain! {
         HttpRequest(reqwest::Error);
     }
 }
-
 
 #[derive(Deserialize, Debug)]
 struct Archive {
@@ -25,13 +25,32 @@ impl Archive {
 }
 
 fn main() -> Result<()> {
-    let mut res = reqwest::blocking::get("https://api.chess.com/pub/player/lgbarn1966/games/archives")?;
+    let matches = App::new("Get games from Chess.com")
+        .version("0.1.0")
+        .author("Luther Barnum")
+        .about("Retrieves games from Chess.com in PGN format")
+        .arg(
+            Arg::with_name("player")
+                .short("p")
+                .long("player")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let  currplayer = matches.value_of("player").unwrap_or("lgbarn");
+
+    let mut res =
+        reqwest::blocking::get("https://api.chess.com/pub/player/lgbarn1966/games/archives")?;
     let mut body = String::new();
     res.read_to_string(&mut body)?;
 
     let deserialized: Archive = serde_json::from_str(&body).unwrap();
 
-    let mut file = File::create("lgbarn1966.pgn")?;
+    let filename = currplayer.to_string() + ".pgn";
+
+    println!("#############{}############", filename);
+
+    let mut file = File::create(filename)?;
 
     for month in deserialized.get_months().iter() {
         let month = format!("{}/pgn", month);
